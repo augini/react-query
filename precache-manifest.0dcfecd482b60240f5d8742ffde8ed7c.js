a9377082551cded705791559edeab817729e25cb
Farrukh <farrukh@Farrukhui-MacBookPro.local>
2020-09-23 06:27:04 +0900

    Updates

---
+++ precache-manifest.0dcfecd482b60240f5d8742ffde8ed7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e595a3383b5130744a4ac6b723cc666",
+    "url": "/react-query/index.html"
+  },
+  {
+    "revision": "d864da1b26146fe9b5db",
+    "url": "/react-query/static/css/main.cca4cc0b.chunk.css"
+  },
+  {
+    "revision": "c9afc8256b3c85f2aa61",
+    "url": "/react-query/static/js/2.83d36092.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-query/static/js/2.83d36092.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d864da1b26146fe9b5db",
+    "url": "/react-query/static/js/main.be049c5e.chunk.js"
+  },
+  {
+    "revision": "e87b8ab2ad1307a4457e",
+    "url": "/react-query/static/js/runtime-main.743906b4.js"
+  }
+]);
